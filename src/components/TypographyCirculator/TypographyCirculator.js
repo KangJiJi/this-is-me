c2fe9ed0fd54984211d35d71e56cef9ce9e80c7a
KangJiJi <kjhmd9726@gmail.com>
2020-07-20 03:13:07 +0900

    [Feature] typography circulator 컴포넌트 구현
    props로 받은 string array의  item을 2.3초에 하나씩 순환하면서 출력한다.

---
+++ src/components/TypographyCirculator/TypographyCirculator.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import "./TypographyCirculator.css";
+
+import useInterval from "../../customHooks/useInterval/useInterval";
+import Typography from "../../atoms/Typography/Typography";
+
+const TypographyCirculator = (props) => {
+  const { stringList, size, color } = props;
+
+  const [currentStringIndex, setCurrentStringIndex] = useState(0);
+
+  const changeStringIndex = () => {
+    if (currentStringIndex === stringList.length - 1) setCurrentStringIndex(0);
+    else setCurrentStringIndex(currentStringIndex + 1);
+  };
+
+  useInterval(changeStringIndex, 2300);
+
+  return (
+    <div className={`typography-circulator`}>
+      <Typography type={"inline"} size={size} color={color}>
+        {stringList[currentStringIndex]}
+      </Typography>
+    </div>
+  );
+};
+
+TypographyCirculator.propTypes = {
+  stringList: PropTypes.arrayOf(PropTypes.string).isRequired,
+  size: PropTypes.oneOf(["default", "medium", "large"]),
+  color: PropTypes.oneOf(["primary", "secondary", "subPrimary", "black"]),
+};
+
+TypographyCirculator.defaultProps = {
+  size: "default",
+  color: "black",
+};
+
+export default TypographyCirculator;
