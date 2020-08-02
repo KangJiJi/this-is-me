caa7fefa38b86138dc3a7e1113058c45de06ba2e
KangJiJi <kjhmd9726@gmail.com>
2020-08-03 01:49:40 +0900

    [Feature] Gallery atom의 반응형 이미지 뷰 기능 추가
    width, height 100%에 맞는게 아닌 화면 넓이에 맞춰서 사이즈가 달라진다.

--- src/atoms/Gallery/Gallery.js
+++ src/atoms/Gallery/Gallery.js
@@ -4,9 +4,9 @@ import PropTypes from "prop-types";
 import "./Gallery.css";
 
 const Gallery = (props) => {
-  const { image, alt, size } = props;
+  const { image, alt, size, responsive } = props;
   return (
-    <div className={`gallery gallery--${size}`}>
+    <div className={`gallery gallery--${responsive ? `responsive` : size}`}>
       <img src={image} alt={alt} />
     </div>
   );
@@ -16,10 +16,12 @@ Gallery.propTypes = {
   image: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
   size: PropTypes.oneOf(["large", "medium", "small"]),
+  responsive: PropTypes.bool,
 };
 
 Gallery.defaultProps = {
   size: "medium",
+  responsive: false,
 };
 
 export default Gallery;
