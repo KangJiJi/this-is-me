a6097c365b56298ef19aacd1a51fd336ada5bc09
KangJiJi <kjhmd9726@gmail.com>
2020-12-22 22:18:59 +0900

    Updates

---
+++ precache-manifest.c1115af3f3f7c829ccc870c7a021c5c3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2884a6e290a9245b97b96c8053f1c80d",
+    "url": "/this-is-me/index.html"
+  },
+  {
+    "revision": "6dc4e7a17e877aeb684d",
+    "url": "/this-is-me/static/css/main.c1b55f53.chunk.css"
+  },
+  {
+    "revision": "d49f17f7d6a7d96d4293",
+    "url": "/this-is-me/static/js/2.dd45f34e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/this-is-me/static/js/2.dd45f34e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc4e7a17e877aeb684d",
+    "url": "/this-is-me/static/js/main.bed115d8.chunk.js"
+  },
+  {
+    "revision": "5d8f4ab3adb6fc0af30c",
+    "url": "/this-is-me/static/js/runtime-main.edeecbb3.js"
+  },
+  {
+    "revision": "287bf52403013a05142384bfe5ca6acf",
+    "url": "/this-is-me/static/media/Amarillo.287bf524.ttf"
+  },
+  {
+    "revision": "a8e5f24c06c6981d316cc34f0ea86200",
+    "url": "/this-is-me/static/media/Amarillo.a8e5f24c.otf"
+  },
+  {
+    "revision": "864494daf5a9b470e2d9876826218ca9",
+    "url": "/this-is-me/static/media/css.864494da.png"
+  },
+  {
+    "revision": "4ae64b73b147bec1be8876c861a4e932",
+    "url": "/this-is-me/static/media/electronTomato.4ae64b73.png"
+  },
+  {
+    "revision": "2241379edd8e7de4a7dec3de19e6bb4b",
+    "url": "/this-is-me/static/media/flappyBird.2241379e.png"
+  },
+  {
+    "revision": "cab02c786f1c6814d7cd10f14d4be1e0",
+    "url": "/this-is-me/static/media/foogle.cab02c78.png"
+  },
+  {
+    "revision": "0918d78648457def080137b179fc5608",
+    "url": "/this-is-me/static/media/github.0918d786.png"
+  },
+  {
+    "revision": "405623509077784122c8743e658d0dd0",
+    "url": "/this-is-me/static/media/html.40562350.png"
+  },
+  {
+    "revision": "0e4fdce8ac22e09688c580e5bc4dcd7d",
+    "url": "/this-is-me/static/media/javascript.0e4fdce8.png"
+  },
+  {
+    "revision": "52642ba5d282c8d715fe0e329d88da68",
+    "url": "/this-is-me/static/media/kakao-talk.52642ba5.png"
+  },
+  {
+    "revision": "9fc27f2a4f4971a85d1eeebfe8664dda",
+    "url": "/this-is-me/static/media/kang.9fc27f2a.jpg"
+  },
+  {
+    "revision": "1de83495e4cb5630d110c76548946614",
+    "url": "/this-is-me/static/media/movieApp.1de83495.png"
+  },
+  {
+    "revision": "5bb712a27bdb509a0a895a7bd8c9ec9e",
+    "url": "/this-is-me/static/media/mozziFront.5bb712a2.png"
+  },
+  {
+    "revision": "731edcfc3098bb04ca1ba41e7a5ae48d",
+    "url": "/this-is-me/static/media/react.731edcfc.png"
+  },
+  {
+    "revision": "7780bccb5cbe855244f4931220d41a8f",
+    "url": "/this-is-me/static/media/southKoreaFlag.7780bccb.png"
+  },
+  {
+    "revision": "2f2aeba69bfa60e0e7a537279b231f9c",
+    "url": "/this-is-me/static/media/thisIsMe.2f2aeba6.png"
+  },
+  {
+    "revision": "d8ba0fb2a631d43223f2bda0a5ed68cb",
+    "url": "/this-is-me/static/media/unitedStatesOfAmericaFlag.d8ba0fb2.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/this-is-me/precache-manifest.c1115af3f3f7c829ccc870c7a021c5c3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/this-is-me/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
