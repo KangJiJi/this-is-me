0d68749239b623ac875766449a99f735a4ecf3ed
KangJi <kjhmd9726@gmail.com>
2021-09-02 04:05:35 +0900

    [Chore] 자격증 업데이트

--- src/components/PersonalInfo/PersonalInfo.js
+++ src/components/PersonalInfo/PersonalInfo.js
@@ -1,84 +1,84 @@
-import React from "react";
+import React from 'react';
 
-import "./PersonalInfo.css";
+import './PersonalInfo.css';
 
-import UserBuilder from "../../objects/user/userBuilder";
+import UserBuilder from '../../objects/user/userBuilder';
 
-import Avatar from "../../atoms/Avatar/Avatar";
-import Label from "../../atoms/Label/Label";
-import List from "../../atoms/List/List";
-import ImageList from "../ImageList/ImageList";
-import ImageListRow from "../../atoms/ImageListRow/ImageListRow";
-import Gallery from "../../atoms/Gallery/Gallery";
+import Avatar from '../../atoms/Avatar/Avatar';
+import Label from '../../atoms/Label/Label';
+import List from '../../atoms/List/List';
+import ImageList from '../ImageList/ImageList';
+import ImageListRow from '../../atoms/ImageListRow/ImageListRow';
+import Gallery from '../../atoms/Gallery/Gallery';
 
-import avatarImage from "../../assets/images/avatar/kang.jpg";
-import kakaoTalk from "../../assets/images/icons/kakao-talk.png";
-import eMail from "../../assets/images/icons/e-mail.png";
-import github from "../../assets/images/icons/github.png";
-import blog from "../../assets/images/icons/blog.png";
-import html from "../../assets/images/techImage/html.png";
-import css from "../../assets/images/techImage/css.png";
-import javascript from "../../assets/images/techImage/javascript.png";
-import react from "../../assets/images/techImage/react.png";
+import avatarImage from '../../assets/images/avatar/kang.jpg';
+import kakaoTalk from '../../assets/images/icons/kakao-talk.png';
+import eMail from '../../assets/images/icons/e-mail.png';
+import github from '../../assets/images/icons/github.png';
+import blog from '../../assets/images/icons/blog.png';
+import html from '../../assets/images/techImage/html.png';
+import css from '../../assets/images/techImage/css.png';
+import javascript from '../../assets/images/techImage/javascript.png';
+import react from '../../assets/images/techImage/react.png';
 
 const PersonalInfo = () => {
   const user = new UserBuilder()
     .avatarImage(avatarImage)
-    .name("강지훈")
-    .nickName("KangJiJi")
+    .name('강지훈')
+    .nickName('KangJiJi')
     .greeting(
       <>
-        안녕하세요. 만나서 반갑습니다. 저는{" "}
+        안녕하세요. 만나서 반갑습니다. 저는{' '}
         <span className={`font-color--secondary-primary-color`}>
           JavaScript
         </span>
-        , <span className={`font-color--secondary-primary-color`}>UI/UX</span>,{" "}
+        , <span className={`font-color--secondary-primary-color`}>UI/UX</span>,{' '}
         <span className={`font-color--secondary-primary-color`}>언어</span>를
-        좋아하는 <span className={`font-color--primary-color`}>Front-end</span>{" "}
+        좋아하는 <span className={`font-color--primary-color`}>Front-end</span>{' '}
         개발자입니다.
         <br />
         <br />
-        많은 사람들과 논리적으로{" "}
+        많은 사람들과 논리적으로{' '}
         <span className={`font-color--secondary-primary-color`}>소통</span>
-        하고 끊임없는{" "}
+        하고 끊임없는{' '}
         <span className={`font-color--secondary-primary-color`}>자기 계발</span>
         을 <span className={`font-color--primary-color`}>지향</span>
         합니다.
       </>
     )
     .educationList([
-      "2016.02 대화고등학교 졸업",
-      "2016.03 서울과학기술대학교 입학",
+      '2016.02 대화고등학교 졸업',
+      '2016.03 서울과학기술대학교 입학',
     ])
     .contactList([
       {
         contactImage: kakaoTalk,
-        imageAlt: "kakao-icon",
-        contents: "pandamuscle",
+        imageAlt: 'kakao-icon',
+        contents: 'pandamuscle',
       },
       {
         contactImage: eMail,
-        imageAlt: "email-icon",
-        contents: "kjhmd9726@gmail.com",
+        imageAlt: 'email-icon',
+        contents: 'kjhmd9726@gmail.com',
       },
       {
         contactImage: github,
-        imageAlt: "github-icon",
-        contents: "github.com/KangJiJi",
+        imageAlt: 'github-icon',
+        contents: 'github.com/KangJiJi',
       },
       {
         contactImage: blog,
-        imageAlt: "blog-icon",
-        contents: "github.com/KangJiJi/Study",
+        imageAlt: 'blog-icon',
+        contents: 'github.com/KangJiJi/Study',
       },
     ])
     .skillList([
-      { skillImage: html, imageAlt: "html-img" },
-      { skillImage: css, imageAlt: "css-img" },
-      { skillImage: javascript, imageAlt: "js-img" },
-      { skillImage: react, imageAlt: "react-img" },
+      { skillImage: html, imageAlt: 'html-img' },
+      { skillImage: css, imageAlt: 'css-img' },
+      { skillImage: javascript, imageAlt: 'js-img' },
+      { skillImage: react, imageAlt: 'react-img' },
     ])
-    .license(["HSK 4级"])
+    .license(['HSK 4级(만료)'])
     .build();
 
   return (
@@ -92,7 +92,7 @@ const PersonalInfo = () => {
             <div className={`personal-info__name personal-info__card`}>
               <span className={`font-color--primary-color`}>
                 {user.getName()}
-              </span>{" "}
+              </span>{' '}
               / {user.getNickName()}
             </div>
             <div className={`personal-info__greeting personal-info__card`}>
